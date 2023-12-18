eb7705eee128b91ab84d196c226b6f0162af6bc4
S-Hinkle <shcatchall@protonmail.com>
2023-12-18 16:55:54 -0500

    added navbar and wallet connect

---
+++ constants/addresses.jsx
@@ -0,0 +1,3 @@
+export const stakeTokenAddress = "";
+export const rewardTokenAddress = "";
+export const stakeContractAddress = "";
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+
+export default function Navbar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start flex-1">
+                <h1>daisyUI</h1>
+            </div>
+            <div className="navbar-center flex-none">
+                <img src="public/images/ReactJS_MVP.png" alt="logo" width={50} />
+                <a className="btn btn-ghost text-xl">DapperFi</a>
+            </div>
+            <div className="navbar-end flex-1 justify-end">
+                <ConnectWallet />
+            </div>
+        </div>
+    )
+}
